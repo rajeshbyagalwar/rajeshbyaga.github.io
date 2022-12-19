287ecf1120628ff59e68bd38c73d5d7d4a8506c5
Rajesh Byagalwar <byagalwarrajesh2001@gmail.com>
2022-12-19 20:31:12 +0530

    Ok done

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,5 +1,5 @@
 import "./about.css";
-
+import  Image from './Image.jpg'
 const About = () => {
   return (
     <div className="a" id="about">
@@ -7,7 +7,7 @@ const About = () => {
         <div className="a-card bg"></div>
         <div className="a-card">
           <img
-            src="https://media.licdn.com/dms/image/D5603AQFmIdM5L3ZPfw/profile-displayphoto-shrink_400_400/0/1669822563884?e=1677110400&v=beta&t=UKekWyXiGBgzV2bwDXMHb_kLDvKnA_wL1Vr85kUn7G8"
+            src={Image}
             alt=""
             className="a-img"
           />
